acc4479e11a96633a451b57f31fdc4bd9d123182
dip23 <adif.maulana@gmail.com>
2019-02-26 09:39:02 +0700

    strukdat-01

--- strukdat-01/exercise-02.cpp
+++ strukdat-01/exercise-02.cpp
@@ -3,7 +3,7 @@
  NPM	:140810180057
  Kelas	:A
  Tanggal Pembuatan :26 Februari 2019
- Nama Program : FizzBuzz
+ Nama Program : konversi suhu celcius ke fahrenheit
  */ 
 
 #include<iostream>
@@ -11,11 +11,11 @@
 using namespace std;
 
 float celtofah(float temp){
-	return ((temp*9)/5)+32;
+	return ((temp*9)/5)+32; //fungsi merubah celcius ke fahrenheit
 }
 
 int main(){
 	float cel;
 	cout<<"Suhu Celcius = ";cin>>cel;
-	cout<<"\nsuhu dalam fahrenheit = "<<celtofah(cel);
+	cout<<"\nsuhu dalam fahrenheit = "<<celtofah(cel); //panggil fungsi konversi
 }
